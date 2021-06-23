46ed5032b4affd80365853e087348e3c4e9fd6ca
Noboru <noboruaso.01@gmail.com>
2021-06-23 18:52:04 -0500

    logo y splash

--- app/src/main/java/com/example/pasaporte_vacunacion/Activities/LoginActivity.java
+++ app/src/main/java/com/example/pasaporte_vacunacion/Activities/LoginActivity.java
@@ -32,6 +32,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SplashTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
